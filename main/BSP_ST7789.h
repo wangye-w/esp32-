c0f5a5e37fc4f33bb70e40bcb3f56327ebdfe433
wangye-w <whr_09@mail.ustc.edu.cn>
2025-08-04 17:45:29 +0000

    对齐

--- main/BSP_ST7789.h
+++ main/BSP_ST7789.h
@@ -8,17 +8,17 @@
 #include "esp_log.h"
 #include "esp_err.h"
 
-#define BSP_LCD_SPI_HOST                    (SPI3_HOST)  // ESP32-S3 的 HSPI
-#define LCD_PIXEL_CLOCK_HZ          (80 * 1000 * 1000)
-#define BSP_LCD_SPI_MOSI            (GPIO_NUM_40)
-#define BSP_LCD_SPI_CLK             (GPIO_NUM_41)
-#define BSP_LCD_SPI_CS              (GPIO_NUM_NC)
-#define BSP_LCD_DC                  (GPIO_NUM_39)
-#define BSP_LCD_RST                 (GPIO_NUM_NC)
-#define BSP_LCD_BCKL                (GPIO_NUM_42)
+#define BSP_LCD_SPI_HOST                (SPI3_HOST)  // ESP32-S3 的 HSPI
+#define BSP_LCD_PIXEL_CLOCK_HZ          (80 * 1000 * 1000)
+#define BSP_LCD_SPI_MOSI                (GPIO_NUM_40)
+#define BSP_LCD_SPI_CLK                 (GPIO_NUM_41)
+#define BSP_LCD_SPI_CS                  (GPIO_NUM_NC)
+#define BSP_LCD_DC                      (GPIO_NUM_39)
+#define BSP_LCD_RST                     (GPIO_NUM_NC)
+#define BSP_LCD_BCKL                    (GPIO_NUM_42)
 
-#define BSP_LCD_H_RES               (320)
-#define BSP_LCD_V_RES               (240)
+#define BSP_LCD_H_RES                   (320)
+#define BSP_LCD_V_RES                   (240)
 
 
 void bsp_st7789_init(void);
